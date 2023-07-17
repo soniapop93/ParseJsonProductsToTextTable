553bf4c5163211a8962101d4be207a5cb94b20be
soniapop93 <popsonia85@gmail.com>
2023-07-17 13:11:46 +0300

    first commit

---
+++ ParseJsonProductsToTextTable/Json/Products.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ParseJsonProductsToTextTable.Json
+{
+    public class Products
+    {
+        public List<Product> products { get; set; }
+
+        public Products(List<Product> products)
+        {
+            this.products = products;
+        }
+
+        private void getProducts()
+        {
+
+        }
+    }
+
+}

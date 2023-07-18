f90bd513827678b09fd60530e2802f678afe089b
soniapop93 <popsonia85@gmail.com>
2023-07-18 17:32:20 +0300

    implemented part that checks the length

--- ParseJsonProductsToTextTable/Text/GenerateTextTable.cs
+++ ParseJsonProductsToTextTable/Text/GenerateTextTable.cs
@@ -15,16 +15,22 @@ namespace ParseJsonProductsToTextTable.Text
             for (int i = 0; i < productsList.Count; i++)
             {
                 itemsLength[0] = productsList[i].id.ToString().Length > itemsLength[0] ? productsList[i].id.ToString().Length : itemsLength[0]; // id
-                itemsLength[1] = productsList[i].title.Length; // title
-                itemsLength[2] = productsList[i].description.Length; // description
-                itemsLength[3] = productsList[i].price.ToString().Length; // price
-                itemsLength[4] = productsList[i].discountPercentage.ToString().Length; // discount Percentage
-                itemsLength[5] = productsList[i].rating.ToString().Length; // rating
-                itemsLength[6] = productsList[i].stock.ToString().Length; // stock
-                itemsLength[7] = productsList[i].brand.Length; // brand
-                itemsLength[8] = productsList[i].category.Length; // category
-                itemsLength[9] = productsList[i].thumbnail.Length; // thumbnail
+                itemsLength[1] = productsList[i].title.Length > itemsLength[1] ? productsList[i].title.Length : itemsLength[1]; // title
+                itemsLength[2] = productsList[i].description.Length > itemsLength[2] ? productsList[i].description.Length : itemsLength[2]; // description
+                itemsLength[3] = productsList[i].price.ToString().Length > itemsLength[3] ? productsList[i].price.ToString().Length : itemsLength[3]; // price
+                itemsLength[4] = productsList[i].discountPercentage.ToString().Length > itemsLength[4] ? productsList[i].discountPercentage.ToString().Length : itemsLength[4]; // discount Percentage
+                itemsLength[5] = productsList[i].rating.ToString().Length > itemsLength[5] ? productsList[i].rating.ToString().Length : itemsLength[5]; // rating
+                itemsLength[6] = productsList[i].stock.ToString().Length > itemsLength[6] ? productsList[i].stock.ToString().Length : itemsLength[6]; // stock
+                itemsLength[7] = productsList[i].brand.Length > itemsLength[7] ? productsList[i].brand.Length : itemsLength[7]; // brand
+                itemsLength[8] = productsList[i].category.Length > itemsLength[8] ? productsList[i].category.Length : itemsLength[8]; // category
+                itemsLength[9] = productsList[i].thumbnail.Length > itemsLength[9] ? productsList[i].thumbnail.Length : itemsLength[9]; // thumbnail
+
             }
+        }
+
+        public void generateColumn(string text, int len)
+        {
+
         }
 
     }
